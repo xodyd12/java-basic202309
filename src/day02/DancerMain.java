7a86fd8a13d66d6b2115e5ca1eda24d64b9129fe
Hantaeyong <hhhhgong3@naver.com>
2023-09-07 14:18:34 +0900

    # 객체지향 기초

---
+++ src/day02/DancerMain.java
@@ -0,0 +1,32 @@
+package day02;
+//실행 클래스:메인메서드를 만들어서 사용
+public class DancerMain {
+
+    public static void main(String[] args) {
+
+        //댄서 객체 생성
+        Dancer kim = new Dancer();
+        //댄서 객체 초기화
+//        kim.dancerName = "김또띠아";
+//        kim.CrewName = "신호등";
+//        kim.genre = "스트릿";
+
+
+        kim.stretch();
+        kim.dance();
+
+        Dancer pack=new Dancer("박격포");
+        pack.introduce();
+        kim.introduce();
+
+        System.out.println("============================");
+        Dancer jang = new Dancer("장동견", "목각");
+        jang.introduce();
+
+        System.out.println("============================");
+        Dancer hong= new Dancer("홍박사", "틱톡커", Genre.KPOP, DanceLevel.AMTEUR);
+        hong.stretch();
+        hong.dance();
+        hong.introduce();
+    }
+}
